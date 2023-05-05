6e4c8852a5faa98c63ffead951e4e6a5fce7195a
ZoranIsailoski <gemini27177@gmail.com>
2023-05-05 21:10:53 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+
+var children = 2;
+var place = 'America';
+var wife = 'Zorana';
+var job = 'Tech CEO';
+
+console.log(`You will be a ${job} in ${place} and married to ${wife} with ${children} kids`);
+  
+// Vtora Zadaca
+
+var birthyear = 2003;
+var futureyear = 2036;
+var result = futureyear - birthyear;
+
+console.log(`If you were born in ${birthyear} then in ${futureyear} you will be either ${result - 1} or ${result} depending on what month it is in ${futureyear}`);
+
+
+// Treta Zadaca
+
+var currentage = 20;
+var maximumage = 90;
+var daily = 30;
+var result = (maximumage - currentage) * 365 * daily;
+
+console.log(`You will need ${result} to last you until the ripe old age of ${maximumage}`)
+
+
+// Cetvrta Zdaca
+
+var radius = 5;
+var c = 2 * 3.14 * radius
+var a = 3.14 * radius * radius
+
+console.log(`The circumference is ${c}\nThe area is ${a} `)
+
+// Petta Zadaca
+
+var celsius = 100;
+var fahrenheit = (celsius * 1.8) + 32;
+
+console.log(`${celsius}C is ${fahrenheit}F`)
+
+fahrenheit = 50;
+celsius = parseFloat(5/9 * (fahrenheit - 32))
+
+console.log(`${fahrenheit}F is ${celsius}C`)
+
+// 5. The Temperature Converter
+// It's hot out! Let's make a converter based on the steps here.
+
+//     Store a celsius temperature into a variable.
+//     Convert it to fahrenheit and output "NN°C is NN°F".
+//     Now store a fahrenheit temperature into a variable.
+//     Convert it to celsius and output "NN°F is NN°C."
+
